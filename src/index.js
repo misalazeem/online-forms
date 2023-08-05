15da4c42ac5238d256c2b4ea8e877154b8a4a994
misalazeem <55094882+misalazeem@users.noreply.github.com>
2023-08-05 16:13:25 +0500

    fixing

--- src/index.js
+++ src/index.js
@@ -1,53 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import React, { Suspense } from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
-import NavBar from './components/navbar';
-import Home from './components/Home';
-import { AuthProvider, useAuth } from './AuthContext';
-import CreateForm from './components/createform';
-import ViewForms from './components/ViewForms';
-import Signup from './components/signup';
-import Login from './components/login';
-import FormView from './components/FormView';
-import AnswerForm from './components/AnswerForm';
-import SendForms from './components/SendForms';
-import SendFormLink from './components/SendFormLink';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { AuthProvider } from './AuthContext';
 
-function AppWrapper() {
-  const { isAuthenticated } = useAuth();
+const root = document.getElementById('root');
 
-  return (
-    <>
-      <NavBar isAuthenticated={isAuthenticated} />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/fill-forms/:formId" element={<AnswerForm />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/login" element={<Login />} />
-        {isAuthenticated ? (
-          <>
-            <Route path="/create-form" element={<CreateForm />} />
-            <Route path="/view-forms" element={<ViewForms />} />
-            <Route path="/forms/:formId" element={<FormView />} />
-            <Route path="/send-forms" element={<SendForms />} />
-            <Route path="/send-form/:formId" element={<SendFormLink />} />
-          </>
-        ) : (
-          <Route path="*" element={<Navigate to="/" />} />
-        )}
-      </Routes>
-    </>
-  );
-}
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthProvider>
-        <AppWrapper />
+        <Suspense fallback={<div>Loading...</div>}>
+          <App />
+        </Suspense>
       </AuthProvider>
     </BrowserRouter>
-  </React.StrictMode>
+  </React.StrictMode>,
+  root
 );
+
+reportWebVitals();
